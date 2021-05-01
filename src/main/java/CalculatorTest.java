581bd88424ab6823ac248dc777c05ccffb36cc06
champossion-svg <1477609091@qq.com>
2021-05-01 12:14:15 +0800

    add calculator

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
     @Test
     public  void shouldReturn0When1Sub1(){
         Calculator calculator = new Calculator();
-        int actualResult =calculator.compute(1,1,'+');
+        int actualResult =calculator.compute(1,1,'-');
         int expectResult = 0;
         Assert.assertEquals(expectResult,actualResult);
     }
